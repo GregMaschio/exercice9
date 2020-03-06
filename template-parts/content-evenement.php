f0e81e3853d71c76191bf88b89577f650dd7dd4e
Gregoire Maschio <gregmaschio@hotmail.com>
2020-03-06 12:38:24 -0500

    completion de la grille evenement

--- template-parts/content-evenement.php
+++ template-parts/content-evenement.php
@@ -14,12 +14,22 @@ $query3 = new WP_Query( $args3 );
 echo '<div class ="conteneur-evenements">';
 // The 2nd Loop
 while ( $query3->have_posts() ) {
-    echo '<div class ="posts-evenements">';
+    // echo '<div class ="posts-evenements">';
     $query3->the_post();
     $jour = get_the_date("j");
     $mois = get_the_date("m");
-    echo '<h3 style = "grid-area:'. $jour . $jour . $mois . $mois .'"> <a href ="'. get_permalink($id) . '">' . get_the_title( $query3->post->ID ) . " </a>- ". get_the_date() . '</h3>';
-    echo '</div>';
+    if($mois == "09"){
+        $mois = 1;
+    }
+    else if($mois == "10"){
+        $mois = 2;
+    }
+    if($mois =="11"){
+        $mois =3;
+    }
+
+    echo '<h3 style = "grid-area:'. $jour . "/" . $mois . "/" . $jour . "/" . $mois .'"> <a href ="'. get_permalink($id) . '">' . get_the_title( $query3->post->ID ) . " </a>- ". get_the_date() . '</h3>';
+    // echo '</div>';
 }
 echo '</div>';
 // Restore original Post Data
